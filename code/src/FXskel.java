c35602f9b9534199932ad8ecebbfcaf98c0ddb05
Joshua <joshua.tite@me.com>
2024-03-18 13:10:30 +0000

    add help page to the game and overall menu optimisation

--- code/src/FXskel.java
+++ code/src/FXskel.java
@@ -1,17 +1,28 @@
+// FXskel.java
+
 import javax.swing.*;
 import java.awt.*;
 
 public class FXskel extends JFrame {
+
+    public FXskel() {
+        initialize();
+    }
+
     public void initialize() {
         setTitle("FXskel");
         setSize(500, 600);
         setMinimumSize(new Dimension(300, 400));
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        // Replace the button with the Menu panel
-        setContentPane(new menu());
+        setContentPane(new menu(this));
 
         setVisible(true);
+    }
+
+    public void showHelpContent() {
+        setContentPane(new HelpPanel());
+        validate(); // Refresh the layout
+        repaint(); // Repaint the frame to show the new content
     }
 }
 
--- code/src/menu.java
+++ code/src/menu.java
@@ -1,16 +1,23 @@
+// menu.java
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class menu extends JPanel {
-    public menu() {
+    private FXskel parentFrame;
+
+    public menu(FXskel parentFrame) {
+        this.parentFrame = parentFrame;
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         setBackground(Color.GRAY);
+
         addMenuButton("Play", e -> System.out.println("Play clicked"));
         addMenuButton("My Character", e -> System.out.println("My Character clicked"));
         addMenuButton("Achievements", e -> System.out.println("Achievements clicked"));
-        addMenuButton("Help", e -> System.out.println("Help clicked"));
+        // Modify the action listener for the "Help" button
+        addMenuButton("Help", e -> parentFrame.showHelpContent());
         addMenuButton("Credits", e -> System.out.println("Credits clicked"));
         addMenuButton("Quit", e -> System.exit(0));
     }
@@ -23,3 +30,4 @@ public class menu extends JPanel {
         add(button);
     }
 }
+
