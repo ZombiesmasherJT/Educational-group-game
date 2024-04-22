475b312155b446b1941948959c54a7dce57f61f0
LordRegent1984 <147600317+LordRegent1984@users.noreply.github.com>
2024-04-22 16:44:35 +0100

    Add files via upload

---
+++ src/main/java/com/example/scenebuilderdemo/Controller.java
@@ -0,0 +1,32 @@
+package com.example.scenebuilderdemo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Controller {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToScene1(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("mainFile.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToScene2(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("helpFile.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ src/main/java/com/example/scenebuilderdemo/Main.java
@@ -0,0 +1,29 @@
+package com.example.scenebuilderdemo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("mainFile.fxml"));
+
+        stage.setTitle("Hydro Heroes: The Quest for Clean Water");
+        stage.setResizable(false);
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
