6fb236e8ecccacee3203ab72996f1072ee0b6ec9
LordRegent1984 <tedimengjezi@hotmail.com>
2024-04-29 13:00:38 +0100

    Version testing

--- src/main/java/com/example/scenebuilderdemo/Controller.java
+++ src/main/java/com/example/scenebuilderdemo/Controller.java
@@ -9,21 +9,47 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Controller {
     private Stage stage;
     private Scene scene;
     private Parent root;
 
-    public void switchToScene1(ActionEvent event) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("mainFile.fxml"));
+    public void switchToMenuPage(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("menuFile.fxml")));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
-    public void switchToScene2(ActionEvent event) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("helpFile.fxml"));
+    public void switchToFirstLevel(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("firstLevelFile.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToHelpPage(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("helpFile.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToMainPage(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("mainFile.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToWorkInProgress(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("workInProgressFile.fxml")));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
