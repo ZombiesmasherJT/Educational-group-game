1fcc0a19dbcef136b011bda4aa468b2d09c475d5
LordRegent1984 <tedimengjezi@hotmail.com>
2024-05-09 16:16:57 +0100

    Added animation to one of the pages to sue as template for other pages

--- src/main/java/com/example/scenebuilderdemo/QuizMenuController.java
+++ src/main/java/com/example/scenebuilderdemo/QuizMenuController.java
@@ -1,5 +1,6 @@
 package com.example.scenebuilderdemo;
 
+import javafx.animation.ScaleTransition;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -10,6 +11,7 @@ import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 public class QuizMenuController {
 
@@ -17,7 +19,12 @@ public class QuizMenuController {
     private Button playquizbtn;
 
     @FXML
+    private Button playinfobtn;
+
+    @FXML
     private void initialize() {
+        setupButtonAnimation(playquizbtn);
+        setupButtonAnimation(playinfobtn);
 
         playquizbtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -39,5 +46,36 @@ public class QuizMenuController {
                 }
             }
         });
+
+        playinfobtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+                try {
+                    Stage thisstage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+                    thisstage.close();
+
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("levelOneInfo.fxml"));
+                    Scene scene = new Scene(fxmlLoader.load());
+                    Stage stage = new Stage();
+                    stage.setScene(scene);
+                    stage.initStyle(StageStyle.TRANSPARENT);
+                    scene.setFill(Color.TRANSPARENT);
+                    stage.show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    private void setupButtonAnimation(Button button) {
+        ScaleTransition st = new ScaleTransition(Duration.millis(200), button);
+        st.setToX(1.1);
+        st.setToY(1.1);
+        st.setCycleCount(2);
+        st.setAutoReverse(true);
+
+        button.setOnMouseEntered(e -> st.playFromStart());
+        button.setOnMouseExited(e -> st.stop());
     }
 }
